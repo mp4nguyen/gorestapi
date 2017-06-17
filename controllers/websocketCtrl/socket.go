e51e9aee04bcc6c61eb66d989717b0802559e9d5
MacOs <macos@MacOss-MacBook-Pro.local>
2017-06-18 01:02:22 +0800

    added websocket

--- controllers/websocketCtrl/socket.go
+++ controllers/websocketCtrl/socket.go
@@ -1,15 +1,24 @@
 package websocketCtrl
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+
+	"bitbucket.org/restapi/utils"
 
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{} // use default options
 
+type socketIn struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
 func Socket(w http.ResponseWriter, r *http.Request) {
+	in := socketIn{}
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
@@ -22,7 +31,10 @@ func Socket(w http.ResponseWriter, r *http.Request) {
 			log.Println("read:", err)
 			break
 		}
-		log.Printf("recv: %s", message)
+		log.Printf("recv: %s\n", message)
+		err = json.Unmarshal(message, &in)
+		log.Println(" in obj = ", in)
+		utils.LogError("Unmarshal socket ", err)
 		err = c.WriteMessage(mt, message)
 		if err != nil {
 			log.Println("write:", err)
